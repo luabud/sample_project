c5591674e36bdb06802e16c7bcafe40fd772aefc
luabud <luabud@microsoft.com>
2021-08-25 15:09:26 -0700

    make it disappear

--- test_pytest.py
+++ test_pytest.py
@@ -1,8 +1,7 @@
-from this import d
 import inc_dec    # The code to test
 
 def test_increment():
     assert inc_dec.increment(3) == 4
 
 def test_decrement():
-    assert inc_dec.decrement(3) == 4
+    assert inc_dec.decrement(5) == 3
