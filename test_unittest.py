7e9bc062db0b99d3a0cd929abb017c422ec76623
luabud <luabud@microsoft.com>
2021-09-28 11:45:20 -0700

    Make a test fail

--- test_unittest.py
+++ test_unittest.py
@@ -7,7 +7,7 @@ class Test_TestIncrementDecrement(unittest.TestCase):
         self.assertEqual(inc_dec.decrement(3), 2)
 
     def test_increment(self):
-        self.assertEqual(inc_dec.increment(3),4)
+        self.assertEqual(inc_dec.increment(3),2)
 
 if __name__ == '__main__':
     unittest.main()
